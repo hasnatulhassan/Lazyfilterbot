a33229acc490a9cfeb128b2701739c7b44f83ad9
Hackingcrews <40152068+hasnatulhassan@users.noreply.github.com>
2022-12-09 19:18:26 +0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,11 @@
-echo "Cloning Repo...."
-git clone https://github.com/hasnatulhassan/Lazyfilterbot.git /Lazyfilterbot
+if [ -z $UPSTREAM_REPO ]
+then
+  echo "Cloning main Repository"
+  git clone https://github.com/hasnatulhassan/Lazyfilterbot.git /Lazyfilterbot
+else
+  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+  git clone $UPSTREAM_REPO /LLazyfilterbot
+fi
 cd /Lazyfilterbot
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
